c41fc3f4f2a1de2a872a8982eaec76e853afc1b2
PengZhang2018 <40973678+PengZhang2018@users.noreply.github.com>
2018-07-09 05:16:25 -0400

    Add files via upload

---
+++ models/My_Calculator.js
@@ -0,0 +1,32 @@
+﻿var myCalculator = function (operator, firstNumber, secondNumber) {
+
+    var operatorValue = operator.replace(/\s+/g, '');
+    var validOperators = ["+", "-", "*", "/"];
+    operatorIsValid = validOperators.indexOf(operatorValue) >= 0;
+
+    if (operatorIsValid == false) {
+        return "Please provide valid operator, only arithmetic operators are supported";
+    }
+
+    if (operatorValue == "+") {
+        return parseFloat(firstNumber + secondNumber).toPrecision(14);
+    }
+
+    if (operatorValue == "-") {
+        return parseFloat(firstNumber - secondNumber).toPrecision(14);
+    }
+
+    if (operatorValue == "*") {
+        return parseFloat(firstNumber * secondNumber).toPrecision(14);
+    }
+
+    if (operatorValue == "/") {
+        if (secondNumber == 0) {
+            return "Numerator cannot be 0";
+        }
+        return parseFloat(firstNumber / secondNumber).toPrecision(14);
+    }
+
+}
+
+module.exports = myCalculator;
